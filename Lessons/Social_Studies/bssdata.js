fff8843800287e95263185a296c6870769c317f1
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2024-05-19 13:16:43 +0200

    Add files via upload

---
+++ Lessons/Social_Studies/bssdata.js
@@ -0,0 +1,17 @@
+// Baby Class lesson plans
+const lessonPlans28 = [
+
+
+// Mathematics
+	
+// Term 1
+	
+// Week 2
+	
+
+	
+	
+	
+	
+];
+
---
+++ Lessons/Social_Studies/g3ssdata.js
@@ -0,0 +1,9 @@
+const lessonPlans33 = [
+
+	
+	
+	
+];
+
+
+
