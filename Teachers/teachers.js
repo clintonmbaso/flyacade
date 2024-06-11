8e74eb19a857b20dd11aa703abeef1c4f9bd6972
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2024-06-11 13:06:45 +0200

    Add files via upload

--- Teachers/teachers.js
+++ Teachers/teachers.js
@@ -17,7 +17,7 @@ var teachers = [
         months: 8,
         qualification: 45,
         attendance: 89,
-        image: "https://www.bing.com/images/search?view=detailV2&ccid=1tznFJyY&id=853B3C9FF8106243DD832547B7B0A5A65C8AC107&thid=OIP.1tznFJyYtUuUz5VziYs2gAHaGo&mediaurl=https%3A%2F%2Fi.pinimg.com%2F736x%2F5b%2Fb8%2Ff1%2F5bb8f1179ae6155dc15875f534dcd741.jpg&cdnurl=https%3A%2F%2Fth.bing.com%2Fth%2Fid%2FR.d6dce7149c98b54b94cf9573898b3680%3Frik%3DB8GKXKalsLdHJQ%26pid%3DImgRaw%26r%3D0&exph=659&expw=736&q=western+province+districts&form=IRPRST&ck=5BCDDE0DB7AEABB56B3C7C7D8628DC96&selectedindex=0&itb=1&ajaxhist=0&ajaxserp=0&vt=2&sim=11&iss=VSI&cit=ccid_ROjgVHb6*cp_414932789956426D0EFEC144EFD78523*mid_9DBAA9831E919123AF7D2FD3195F677ED57D8E90*thid_OIP.ROjgVHb6apJ4zLu!_kTOaGQHaGo"
+        image: "https://1drv.ms/i/s!AhLdauWY6gvBg2nU3sVbYi4vXWAE=1"   
     },
     {
         id: 3,
@@ -27,7 +27,7 @@ var teachers = [
         months: 2,
         qualification: 45,
         attendance: 87,
-        image: "teacher1.jpg"
+        image: "https://1drv.ms/i/s!AhLdauWY6gvBg2nU3sVbYi4vXWAE=1"
     },
     {
         id: 4,
@@ -37,6 +37,6 @@ var teachers = [
         months: 56,
         qualification: 80,
         attendance: 93,
-        image: "https://1drv.ms/i/s!AhLdauWY6gvBg2nU3sVbYi4vXWAE"
+        image: "https://1drv.ms/i/s!AhLdauWY6gvBg2nU3sVbYi4vXWAE=1"
     }
 ];
