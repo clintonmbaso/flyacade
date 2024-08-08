dedb3692878f06846a8754d50ae0b09fb0d50f3f
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2024-08-08 19:09:13 +0200

    Add files via upload

--- photo.js
+++ photo.js
@@ -1,12 +1,19 @@
 const imageData = [
  {
-        url: 'https://raw.githubusercontent.com/clintonmbaso/subilo/main/images/Mbaso.jpg',
+        url: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/PSX_20240808_184202.jpg',
         alt: 'Mbaso Image'
     },
     // Add more image objects here
     // Example:
     {
-         url: 'http://flyacade.com.preview.services/images/767048.jpg',
+         url: 'http://flyacade.com.preview.services/images/tour1/PSX_20240808_184202.jpg',
          alt: 'Image 2'
-     }
+     },
+    {
+         url: 'https://drive.google.com/file/d/1-hs_wHZ7vMzIqf24LIs4Sp_qopLW7Yzm/view?usp=drivesdk',
+         alt: 'Image 3'
+     },
+
+
+
 ];
