bd59f244507646a5bdee11f191db0d2ee486a955
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2024-08-09 22:27:25 +0200

    Add files via upload

--- photo.js
+++ photo.js
@@ -49,5 +49,159 @@ const imageData = [
         downloadUrl: 'https://drive.google.com/file/d/1CUJijkI_-wke3DXMJkgLuNb_myESz95c/view?usp=drivesdk',
         alt: 'Mbaso Image'
     },
+
+
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0035.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1ClSmbIsmiOb5tvmiu8Umc0OxjqycVumz/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0036.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1ClSmbIsmiOb5tvmiu8Umc0OxjqycVumz/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0037.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1CvMl1DxXZ5aLiBY-BN1_tWfHSp-6Eldz/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0038.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1CvsHE1QqmMMVpubFDZnnODwymE6-qT2S/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0039.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1CwZR6DSU_v3sg6r15K3KHVDMSkM-PWSY/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0040.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1CwkTAdy0SB_KzCG_5PntWHLiI6IztS0c/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0041.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1Cxd0vffT97v3q-eUs5fYVKJY55Bnso2i/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0042.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1D-W3oJo4Nl1JXigf9aDwfCjLoAKoSXwZ/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0043.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1D7nHNszL5MGjql5EJIYTYGPPwP-c3JuY/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0044.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1D9QaWY-iNCmnWzcdwkJj585f_vQYI5Ay/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+
+
+
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0045.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1D9uzLJyc7bf6-QI7CBtTshrUEZ4jtP2H/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0046.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1DDLgsEwGjVsOke1KMyQLJJ0GD65XwOKn/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0047.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1DFY9lbsQF9Fgjx6fPYKMl_LSaxhuR9uU/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0048.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1DGLToZlqNIHqmiRdxnY2TuROvqyPEkOT/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0049.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1DI0T_8fAk12ahWEevbcZ0MuRbV0dGiHc/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0050.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1DLMZmoLQ8Fh51P3V0PgiqKhYnptiAO8Y/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0051.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1DPCxcv_KeCydt4E7CsvF_fujYViihuwf/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0052.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1DQAoMgfHreJ_lrpkNwwd5FTqyuO8BSXM/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0053.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1DSzsofySs5-6nhPh4ZQb-tH3MjQQtGWg/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0054.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1DXy5-qoHeSUtppHNb5qZw8Uyoy3V9boJ/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+
+
+
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0055.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1D_A-8BVhWqIqDEmppwtsm4gpBONpt90T/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0056.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1DjVvPkfNe7dtH8ZBTpe8LNt42JtYWKVm/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0057.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1DpKvzcZTbqnnAbJ4k6TdTzjoshSR4PQS/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0058.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1Dycugm6HIELyYHtboHW7EwXhkFjbx9SN/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0059.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1E3qTfDjQMwiaXvUKowD9FRCSOeeCiCsT/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0060.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1E4n1YRSuuwfKiWjXmwpCUAMuOaviT6tL/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0061.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1EJJeWzgeQ4gyDx5AB2kBLzkpq7Ii6hkZ/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0062.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1EMvlZxhKQVrMQVLk-8eUMaiAoZlcQeqV/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+    {
+        viewUrl: 'https://raw.githubusercontent.com/clintonmbaso/flyacade/main/images/tour1/IMG-20240809-WA0063.jpg',
+        downloadUrl: 'https://drive.google.com/file/d/1EOj8HSEhi5hcuyDR1PTBzV3LhRf21DiX/view?usp=drivesdk',
+        alt: 'Mbaso Image'
+    },
+
     // Add more image objects with viewUrl and downloadUrl here
 ];
