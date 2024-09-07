652eea4e698769ef48fe4b4617aaca9d59159aa1
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2024-09-07 14:50:16 +0200

    Add files via upload

---
+++ Lessons/Expressive_Arts/meadata1.js
@@ -0,0 +1,85 @@
+const lessonPlans38a = [
+
+// Term 1
+// Week 2
+// Lesson 1
+
+
+// Lesson 2
+
+
+// Lesson 3
+
+
+// Week 3
+// Lesson 1
+
+
+// Lesson 2
+
+
+// Lesson 3
+
+
+// Week 4
+// Lesson 1
+
+
+// Lesson 2
+
+
+// Lesson 3
+
+
+// Week 5
+// Lesson 1
+
+
+// Lesson 2
+
+
+// Lesson 3
+
+
+// Week 7
+// Lesson 1
+
+
+// Lesson 2
+
+
+// Lesson 3
+
+
+// Week 8
+// Lesson 1
+
+
+// Lesson 2
+
+
+// Lesson 3
+
+
+// Week 9
+// Lesson 1
+
+
+// Lesson 2
+
+
+// Lesson 3
+
+
+// Week 10
+// Lesson 1
+
+
+// Lesson 2
+
+
+// Lesson 3
+
+	
+	
+];
