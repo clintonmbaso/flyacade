249d211be5b270e30afa816d660ac4984a8a629d
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2024-09-20 13:58:39 +0200

    Add files via upload

---
+++ Tests/sp1/grade1/g1scit2afillInTheBlankQuestions.js
@@ -0,0 +1,164 @@
+const g1scit2afillInTheBlankQuestions = [
+{
+"type": "fill_in_the_blank",
+"class": "Feather Flyers",
+"subject": "Science",
+"term": "Term 2",
+"part": "Mid Term",
+"question": "The wooden block is made of __________.",
+"answer": "wood"
+},
+{
+"type": "fill_in_the_blank",
+"class": "Feather Flyers",
+"subject": "Science",
+"term": "Term 2",
+"part": "Mid Term",
+"question": "The metal spoon is made of __________.",
+"answer": "metal"
+},
+{
+"type": "fill_in_the_blank",
+"class": "Feather Flyers",
+"subject": "Science",
+"term": "Term 2",
+"part": "Mid Term",
+"question": "The plastic bottle is made of __________.",
+"answer": "plastic"
+},
+{
+"type": "fill_in_the_blank",
+"class": "Feather Flyers",
+"subject": "Science",
+"term": "Term 2",
+"part": "Mid Term",
+"question": "The cloth napkin is made of __________.",
+"answer": "cloth"
+},
+{
+"type": "fill_in_the_blank",
+"class": "Feather Flyers",
+"subject": "Science",
+"term": "Term 2",
+"part": "Mid Term",
+"question": "The wooden block is __________ to touch.",
+"answer": "smooth"
+},
+{
+"type": "fill_in_the_blank",
+"class": "Feather Flyers",
+"subject": "Science",
+"term": "Term 2",
+"part": "Mid Term",
+"question": "The metal spoon is __________ to touch.",
+"answer": "cold"
+},
+{
+"type": "fill_in_the_blank",
+"class": "Feather Flyers",
+"subject": "Science",
+"term": "Term 2",
+"part": "Mid Term",
+"question": "The plastic bottle is __________.",
+"answer": "flexible"
+},
+{
+"type": "fill_in_the_blank",
+"class": "Feather Flyers",
+"subject": "Science",
+"term": "Term 2",
+"part": "Mid Term",
+"question": "The cloth napkin is __________.",
+"answer": "soft"
+},
+{
+  "type": "fill_in_the_blank",
+  "class": "Feather Flyers",
+  "subject": "Science",
+  "term": "Term 2",
+  "part": "Mid Term",
+  "question": "The wooden block is made of __________.",
+  "answer": "wood"
+},
+{
+  "type": "fill_in_the_blank",
+  "class": "Feather Flyers",
+  "subject": "Science",
+  "term": "Term 2",
+  "part": "Mid Term",
+  "question": "The metal spoon is made of __________.",
+  "answer": "metal"
+},
+{
+  "type": "fill_in_the_blank",
+  "class": "Feather Flyers",
+  "subject": "Science",
+  "term": "Term 2",
+  "part": "Mid Term",
+  "question": "The plastic bottle is made of __________.",
+  "answer": "plastic"
+},
+{
+  "type": "fill_in_the_blank",
+  "class": "Feather Flyers",
+  "subject": "Science",
+  "term": "Term 2",
+  "part": "Mid Term",
+  "question": "The cloth napkin is made of __________.",
+  "answer": "cloth"
+},
+{
+  "type": "fill_in_the_blank",
+  "class": "Feather Flyers",
+  "subject": "Science",
+  "term": "Term 2",
+  "part": "Mid Term",
+  "question": "The wooden block is __________ to touch.",
+  "answer": "rough"
+},
+{
+  "type": "fill_in_the_blank",
+  "class": "Feather Flyers",
+  "subject": "Science",
+  "term": "Term 2",
+  "part": "Mid Term",
+  "question": "The metal spoon is __________.",
+  "answer": "smooth"
+},
+{
+  "type": "fill_in_the_blank",
+  "class": "Feather Flyers",
+  "subject": "Science",
+  "term": "Term 2",
+  "part": "Mid Term",
+  "question": "The plastic bottle is made of __________.",
+  "answer": "plastic"
+},
+{
+  "type": "fill_in_the_blank",
+  "class": "Feather Flyers",
+  "subject": "Science",
+  "term": "Term 2",
+  "part": "Mid Term",
+  "question": "The cloth napkin is made of __________.",
+  "answer": "cloth"
+},
+{
+  "type": "fill_in_the_blank",
+  "class": "Feather Flyers",
+  "subject": "Science",
+  "term": "Term 2",
+  "part": "Mid Term",
+  "question": "The wooden block is __________.",
+  "answer": "solid"
+},
+{
+  "type": "fill_in_the_blank",
+  "class": "Feather Flyers",
+  "subject": "Science",
+  "term": "Term 2",
+  "part": "Mid Term",
+  "question": "The metal spoon is __________ to touch.",
+  "answer": "cool"
+}
+];
