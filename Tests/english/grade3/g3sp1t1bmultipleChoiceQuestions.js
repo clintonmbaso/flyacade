e8052dc3bf083457d7314aca66cfaf65f50e6a8a
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2024-09-20 13:32:17 +0200

    Add files via upload

---
+++ Tests/english/grade3/g3sp1t1bmultipleChoiceQuestions.js
@@ -0,0 +1,12 @@
+const g3sp1t1bmultipleChoiceQuestions = [
+  {
+    type: "multiple_choice",
+    class: "Wing Whiz",
+    subject: "Special Paper 1",
+    term: "Term 1",
+    part: "End Term",
+    question: "Dog is to collar as human is to ____",
+    choices: ["Shirt", "Hat", "Shoes", "Necklace"],
+    answer: "Necklace"
+  }
+];
---
+++ Tests/english/grade3/g3sp1t3amultipleChoiceQuestions.js
@@ -0,0 +1,12 @@
+const g3sp1t3amultipleChoiceQuestions = [
+  {
+    type: "multiple_choice",
+    class: "Wing Whiz",
+    subject: "Special Paper 1",
+    term: "Term 3",
+    part: "Mid Term",
+    question: "Dog is to collar as human is to ____",
+    choices: ["Shirt", "Hat", "Shoes", "Necklace"],
+    answer: "Necklace"
+  }
+];
