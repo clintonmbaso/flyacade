f7f357912549913a47b295d48989e4835a54f2ca
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2024-09-20 16:08:35 +0200

    Add files via upload

---
+++ Tests/ss/grade5/g5sst3atrueFalseQuestions.js
@@ -0,0 +1,11 @@
+const g5sst3atrueFalseQuestions = [
+  {
+    type: "true_false",
+    class: "Nestlings Navigators",
+    subject: "Science",
+    term: "Term 1",
+    part: "Mid Term",
+    question: "The Earth is flat.",
+    answer: "false"
+  },
+];
