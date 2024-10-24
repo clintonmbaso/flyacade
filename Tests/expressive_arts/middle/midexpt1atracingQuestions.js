d9bd1ef96bd7ee202fdf6ab2de0d8a2b64c986ae
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2024-10-24 10:13:59 +0200

    Add files via upload

---
+++ Tests/expressive_arts/middle/midexpt1atracingQuestions.js
@@ -0,0 +1,70 @@
+const midexpt1atracingQuestions = [
+{
+  type: "tracing",
+  class: "Sky Sprouts",
+  subject: "Expressive Arts",
+  term: "Term 1",
+  part: "Mid Term",
+  question: "Trace the following shape carefully:",
+  tracingContent: "rectangle",
+  },
+{
+  type: "tracing",
+  class: "Sky Sprouts",
+  subject: "Expressive Arts",
+  term: "Term 1",
+  part: "Mid Term",
+  question: "Trace the following shape carefully:",
+  tracingContent: "square"
+},
+{
+  type: "tracing",
+  class: "Sky Sprouts",
+  subject: "Expressive Arts",
+  term: "Term 1",
+  part: "Mid Term",
+  question: "Trace the following shape carefully:",
+  tracingContent: "triangle",
+  },
+{
+  type: "tracing",
+  class: "Sky Sprouts",
+  subject: "Expressive Arts",
+  term: "Term 1",
+  part: "Mid Term",
+  question: "Trace the following shape carefully:",
+  tracingContent: "circle"
+},
+{
+  type: "tracing",
+  class: "Sky Sprouts",
+  subject: "Expressive Arts",
+  term: "Term 1",
+  part: "Mid Term",
+  question: "Trace the following shape carefully:",
+  tracingContent: "oval"
+  },
+
+
+// Images
+{
+  type: "tracing",
+  class: "Sky Sprouts",
+  subject: "Expressive Arts",
+  term: "Term 1",
+  part: "Mid Term",
+  tracingType: "image",
+  question: "Trace the outline of the banana.",
+  tracingImage: "images/science/banana.png"
+},
+{
+  type: "tracing",
+  class: "Sky Sprouts",
+  subject: "Expressive Arts",
+  term: "Term 1",
+  part: "Mid Term",
+  tracingType: "image",
+  question: "Trace the outline of the apple.",
+  tracingImage: "images/science/apple.png"
+  }
+];
