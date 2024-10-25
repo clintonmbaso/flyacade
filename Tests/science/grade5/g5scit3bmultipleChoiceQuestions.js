ece19cc492fcd8fb7b4337b0ebab95b2fe6a81e1
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2024-10-25 16:02:04 +0200

    Add files via upload

--- Tests/science/grade5/g5scit3bmultipleChoiceQuestions.js
+++ Tests/science/grade5/g5scit3bmultipleChoiceQuestions.js
@@ -1,12 +1,3 @@
 const g5scit3bmultipleChoiceQuestions = [
-{
-  "type": "multiple_choice",
-  "class": "Avian Aces",
-  "subject": "Science",
-  "term": "Term 3",
-  "part": "End Term",
-  "question": "What material is the wooden block made of?",
-  "choices": ["Wood", "Metal", "Plastic", "Sticks"],
-  "answer": "Wood"
-}
+
 ];
