91f186e47b99530d0b13e9d1425020b92a5af30a
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2024-10-25 13:04:04 +0200

    Add files via upload

--- Tests/literacy/middle/midlitt3breadingQuestions.js
+++ Tests/literacy/middle/midlitt3breadingQuestions.js
@@ -1,47 +1,42 @@
 const midlitt3breadingQuestions = [
 {
      type: "reading",
+     subtype: "words",
      class: "Sky Sprouts",
      subject: "Literacy",
      term: "Term 3",
      part: "End Term",
-    question: "Read the following words by sounding.",
+    question: "Read the following words starting with sound 'o'.",
     readingContent: ["on", "of", "or"]
 },
 {
   type: "reading",
+     subtype: "words",
      class: "Sky Sprouts",
      subject: "Literacy",
      term: "Term 3",
      part: "End Term",  
-    question: "Read the following words by sounding.",
+    question: "Read the following words starting with sound 'a'.",
     readingContent: ["an", "am", "as", "at"]
 },
 {
   type: "reading",
+     subtype: "words",
      class: "Sky Sprouts",
      subject: "Literacy",
      term: "Term 3",
      part: "End Term",  
-    question: "Read the following words by sounding.",
+    question: "Read the following words starting with sound 'i'.",
     readingContent: ["it", "if", "in", "is"]
 },
 {
   type: "reading",
+     subtype: "words",
      class: "Sky Sprouts",
      subject: "Literacy",
      term: "Term 3",
      part: "End Term",  
     question: "Read the following words starting with sound 'u'.",
     readingContent: ["un", "up", "us"]
-},
-{
-  type: "reading",
-     class: "Sky Sprouts",
-     subject: "Literacy",
-     term: "Term 3",
-     part: "End Term",  
-    question: "Read the following words by sounding.",
-    readingContent: ["of", "on", "or"]
-},
+}
 ];
