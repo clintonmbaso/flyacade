02df92da9c7f974a8d3c004c2ba5e5884d209ea9
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2024-10-25 13:01:12 +0200

    Add files via upload

---
+++ Tests/literacy/reception/reclitt3areadingQuestions.js
@@ -0,0 +1,85 @@
+const reclitt3breadingQuestions = [
+{
+  type: "reading",
+     subtype: "words",
+     class: "Nestlings Navigators",
+     subject: "Literacy",
+     term: "Term 3",
+     part: "Mid Term",  
+    question: "Read the following CVC words starting with vowel 'a'.",
+    readingContent: ["can", "ram", "sat", "dat", "tap"]
+},
+{
+  type: "reading",
+     subtype: "words",
+     class: "Nestlings Navigators",
+     subject: "Literacy",
+     term: "Term 3",
+     part: "Mid Term",  
+    question: "Read the following CVC words starting with vowel 'a'.",
+    readingContent: ["can", "ram", "sat", "dat", "tap"]
+},
+{
+  type: "reading",
+     subtype: "words",
+     class: "Nestlings Navigators",
+     subject: "Literacy",
+     term: "Term 3",
+     part: "Mid Term",  
+    question: "Read the following CVC words starting with vowel 'a'.",
+    readingContent: ["can", "ram", "sat", "dat", "tap"]
+},
+{
+  type: "reading",
+     subtype: "words",
+     class: "Nestlings Navigators",
+     subject: "Literacy",
+     term: "Term 3",
+     part: "Mid Term",  
+    question: "Read the following CVC words starting with vowel 'a'.",
+    readingContent: ["can", "ram", "sat", "dat", "tap"]
+},
+{
+  type: "reading",
+     subtype: "words",
+     class: "Nestlings Navigators",
+     subject: "Literacy",
+     term: "Term 3",
+     part: "Mid Term",  
+    question: "Read the following CVC words starting with vowel 'a'.",
+    readingContent: ["can", "ram", "sat", "dat", "tap"]
+},
+
+
+{
+     type: "reading",
+     subtype: "words",
+     class: "Nestlings Navigators",
+     subject: "Literacy",
+     term: "Term 3",
+     part: "Mid Term",
+    question: "Read the following CVC words starting with vowel 'o'.",
+    readingContent: ["cob", "rob", "nob", "pot", "won"]
+},
+
+{
+  type: "reading",
+     subtype: "words",
+     class: "Nestlings Navigators",
+     subject: "Literacy",
+     term: "Term 3",
+     part: "Mid Term",  
+    question: "Read the following CVC words starting with vowel 'i'.",
+    readingContent: ["bit", "min", "pin", "tip", "win"]
+},
+{
+  type: "reading",
+     subtype: "words",
+     class: "Nestlings Navigators",
+     subject: "Literacy",
+     term: "Term 3",
+     part: "Mid Term",  
+    question: "Read the following CVC words starting with vowel 'u'.",
+    readingContent: ["sun", "cup", "bus", "cut", "dug"]
+}
+];
