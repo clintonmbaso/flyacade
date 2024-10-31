f93df5c231195a28091a3085cdc3eb9aa89cd45c
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2024-10-31 21:37:51 +0200

    Add files via upload

---
+++ Tests/english/baby/babyengt3bidentificationQuestions.js
@@ -0,0 +1,55 @@
+const babyengt3bidentificationQuestions = [
+    {
+        type: "identification",
+        class: "Baby Birds",
+        subject: "English",
+        term: "Term 3",
+        part: "End Term",
+        question: "Identify the letter shown below",
+        subtype: "letter",
+        value: "Ww"
+    },
+    {
+        type: "identification",
+        class: "Baby Birds",
+        subject: "English",
+        term: "Term 3",
+        part: "End Term",
+        question: "Identify the letter shown below",
+        subtype: "letter",
+        value: "Xx"
+    },
+    {
+        type: "identification",
+        class: "Baby Birds",
+        subject: "English",
+        term: "Term 3",
+        part: "End Term",
+        question: "Identify the letter shown below",
+        subtype: "letter",
+        value: "Yy"
+    },
+    {
+        type: "identification",
+        class: "Baby Birds",
+        subject: "English",
+        term: "Term 3",
+        part: "End Term",
+        question: "Identify the letter shown below",
+        subtype: "letter",
+        value: "Zz"
+    }
+
+/*
+    {
+        type: "identification",
+        class: "Baby Birds",
+        subject: "English",
+        term: "Term 3",
+        part: "End Term",
+        question: "identify the shape shown below",
+        subtype: "shape",
+        imgSrc: "images/shapes/circle.png"
+    }
+*/
+];
---
+++ Tests/english/baby/babyengt3bmultipleChoiceQuestions.js
@@ -0,0 +1,82 @@
+const midengt3bmultipleChoiceQuestions = [
+  {
+    "type": "multiple_choice",
+    "class": "Sky Sprouts",
+    "subject": "English",
+    "term": "Term 3",
+    "part": "End Term",
+    "question": "Walter Walrus says _________.",
+    "choices": ["Ww", "Xx", "Yy", "Zz"],
+    "answer": "Ww"
+  },
+  {
+    "type": "multiple_choice",
+    "class": "Sky Sprouts",
+    "subject": "English",
+    "term": "Term 3",
+    "part": "End Term",
+    "question": "Fix it Max says _________.",
+    "choices": ["Ww", "Xx", "Yy", "Zz"],
+    "answer": "Xx"
+  },
+  {
+    "type": "multiple_choice",
+    "class": "Sky Sprouts",
+    "subject": "English",
+    "term": "Term 3",
+    "part": "End Term",
+    "question": "Yellow Yoyoman says _________.",
+    "choices": ["Ww", "Xx", "Yy", "Zz"],
+    "answer": "Yy"
+  },
+  {
+    "type": "multiple_choice",
+    "class": "Sky Sprouts",
+    "subject": "English",
+    "term": "Term 3",
+    "part": "End Term",
+    "question": "Zigzag Zebra says _________.",
+    "choices": ["Ww", "Xx", "Yy", "Zz"],
+    "answer": "Zz"
+  },
+  {
+    "type": "multiple_choice",
+    "class": "Sky Sprouts",
+    "subject": "English",
+    "term": "Term 3",
+    "part": "End Term",
+    "question": "Sound 'Rr' for _________.",
+    "choices": ["Walter Walrus", "Fix it Max", "Yellow Yoyoman", "Zigzag Zebra"],
+    "answer": "Walter Walrus"
+  },
+  {
+    "type": "multiple_choice",
+    "class": "Sky Sprouts",
+    "subject": "English",
+    "term": "Term 3",
+    "part": "End Term",
+    "question": "Sound 'Ss' for _________.",
+    "choices": ["Walter Walrus", "Fix it Max", "Yellow Yoyoman", "Zigzag Zebra"],
+    "answer": "Fix it Max"
+  },
+  {
+    "type": "multiple_choice",
+    "class": "Sky Sprouts",
+    "subject": "English",
+    "term": "Term 3",
+    "part": "End Term",
+    "question": "Sound 'Tt' for _________.",
+    "choices": ["Walter Walrus", "Fix it Max", "Yellow Yoyoman", "Zigzag Zebra"],
+    "answer": "Yellow Yoyoman"
+  },
+  {
+    "type": "multiple_choice",
+    "class": "Sky Sprouts",
+    "subject": "English",
+    "term": "Term 3",
+    "part": "End Term",
+    "question": "Sound 'Vv' for _________.",
+    "choices": ["Walter Walrus", "Fix it Max", "Yellow Yoyoman", "Zigzag Zebra"],
+    "answer": "Zigzag Zebra"
+  }
+]
