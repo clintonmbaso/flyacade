c48fb60a4af8f4a14e3f0100decdfbe3bf4e141c
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2024-11-04 15:36:15 +0200

    Add files via upload

---
+++ Tests/math/baby/babymatht3bidentityQuestions.js
@@ -0,0 +1,123 @@
+const babymatht3bidentityQuestions = [
+    {
+        type: "identity",
+        class: "Baby Birds",
+        subject: "Mathematics",
+        term: "Term 3",
+        part: "End Term",
+        question: "Circle the given number from the following options:",
+        identityPrompt: "Number 0",
+        identityTarget: "0",
+        identityContent: ["1", "0", "1", "0", "1", "0", "1", "0", "0", "1"]
+    },
+    {
+        type: "identity",
+        class: "Baby Birds",
+        subject: "Mathematics",
+        term: "Term 3",
+        part: "End Term",
+        question: "Circle the given number from the following options:",
+        identityPrompt: "Number 1",
+        identityTarget: "1",
+        identityContent: ["1", "2", "1", "1", "1", "2", "1", "2", "2", "2"]
+    },
+    {
+        type: "identity",
+        class: "Baby Birds",
+        subject: "Mathematics",
+        term: "Term 3",
+        part: "End Term",
+        question: "Circle the given number from the following options:",
+        identityPrompt: "Number 2",
+        identityTarget: "2",
+        identityContent: ["3", "2", "2", "3", "3", "3", "2", "3", "2", "2"]
+    },
+    {
+        type: "identity",
+        class: "Baby Birds",
+        subject: "Mathematics",
+        term: "Term 3",
+        part: "End Term",
+        question: "Circle the given number from the following options:",
+        identityPrompt: "Number 3",
+        identityTarget: "3",
+        identityContent: ["3", "4", "4", "3", "3", "3", "4", "3", "4", "4"]
+    },
+    {
+        type: "identity",
+        class: "Baby Birds",
+        subject: "Mathematics",
+        term: "Term 3",
+        part: "End Term",
+        question: "Circle the given number from the following options:",
+        identityPrompt: "Number 4",
+        identityTarget: "4",
+        identityContent: ["4", "4", "5", "4", "4", "5", "5", "4", "5", "5"]
+    },
+    {
+        type: "identity",
+        class: "Baby Birds",
+        subject: "Mathematics",
+        term: "Term 3",
+        part: "End Term",
+        question: "Circle the given number from the following options:",
+        identityPrompt: "Number 5",
+        identityTarget: "5",
+        identityContent: ["5", "6", "5", "5", "5", "6", "6", "5", "6", "6"]
+    },
+    {
+        type: "identity",
+        class: "Baby Birds",
+        subject: "Mathematics",
+        term: "Term 3",
+        part: "End Term",
+        question: "Circle the given number from the following options:",
+        identityPrompt: "Number 6",
+        identityTarget: "6",
+        identityContent: ["4", "6", "6", "4", "4", "6", "4", "6", "4", "6"]
+    },
+    {
+        type: "identity",
+        class: "Baby Birds",
+        subject: "Mathematics",
+        term: "Term 3",
+        part: "End Term",
+        question: "Circle the given number from the following options:",
+        identityPrompt: "Number 7",
+        identityTarget: "7",
+        identityContent: ["3", "3", "7", "3", "7", "7", "3", "7", "7", "3"]
+    },
+    {
+        type: "identity",
+        class: "Baby Birds",
+        subject: "Mathematics",
+        term: "Term 3",
+        part: "End Term",
+        question: "Circle the given number from the following options:",
+        identityPrompt: "Number 8",
+        identityTarget: "8",
+        identityContent: ["7", "8", "8", "8", "7", "7", "8", "8", "7", "7"]
+    },
+    {
+        type: "identity",
+        class: "Baby Birds",
+        subject: "Mathematics",
+        term: "Term 3",
+        part: "End Term",
+        question: "Circle the given number from the following options:",
+        identityPrompt: "Number 9",
+        identityTarget: "9",
+        identityContent: ["9", "9", "8", "8", "8", "9", "9", "9", "8", "8"]
+    },
+    {
+        type: "identity",
+        class: "Baby Birds",
+        subject: "Mathematics",
+        term: "Term 3",
+        part: "End Term",
+        question: "Circle the given number from the following options:",
+        identityPrompt: "Number 10",
+        identityTarget: "10",
+        identityContent: ["9", "9", "10", "10", "10", "9", "10", "10", "9", "9"]
+    }
+];
