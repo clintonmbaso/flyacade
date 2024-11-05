04e5faa7a4371216d099f8632af73035846e45ee
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2024-11-05 02:21:26 +0200

    Add files via upload

---
+++ Tests/sp1/grade2/g2sp1t3bfillInTheBlankQuestions.js
@@ -0,0 +1,220 @@
+const g2sp1t3bfillInTheBlankQuestions = [
+  {
+    type: "fill_in_the_blank",
+    class: "Cloud Chicks",
+    subject: "Special Paper 1",
+    term: "Term 3",
+    part: "End Term",
+    question: "A week has ______ days.",
+    answer: "7"
+  },
+  {
+    type: "fill_in_the_blank",
+    class: "Cloud Chicks",
+    subject: "Special Paper 1",
+    term: "Term 3",
+    part: "End Term",
+    question: "A year has ______ months.",
+    answer: "12"
+  },
+  {
+    type: "fill_in_the_blank",
+    class: "Cloud Chicks",
+    subject: "Special Paper 1",
+    term: "Term 3",
+    part: "End Term",
+    question: "January ALWAYS has ______ days.",
+    answer: "31"
+  },
+  {
+    type: "fill_in_the_blank",
+    class: "Cloud Chicks",
+    subject: "Special Paper 1",
+    term: "Term 3",
+    part: "End Term",
+    question: "March ALWAYS has ______ days.",
+    answer: "31"
+  },
+  {
+    type: "fill_in_the_blank",
+    class: "Cloud Chicks",
+    subject: "Special Paper 1",
+    term: "Term 3",
+    part: "End Term",
+    question: "April ALWAYS has ______ days.",
+    answer: "30"
+  },
+  {
+    type: "fill_in_the_blank",
+    class: "Cloud Chicks",
+    subject: "Special Paper 1",
+    term: "Term 3",
+    part: "End Term",
+    question: "May ALWAYS has ______ days.",
+    answer: "31"
+  },
+  {
+    type: "fill_in_the_blank",
+    class: "Cloud Chicks",
+    subject: "Special Paper 1",
+    term: "Term 3",
+    part: "End Term",
+    question: "June ALWAYS has ______ days.",
+    answer: "30"
+  },
+  {
+    type: "fill_in_the_blank",
+    class: "Cloud Chicks",
+    subject: "Special Paper 1",
+    term: "Term 3",
+    part: "End Term",
+    question: "July ALWAYS has ______ days.",
+    answer: "31"
+  },
+  {
+    type: "fill_in_the_blank",
+    class: "Cloud Chicks",
+    subject: "Special Paper 1",
+    term: "Term 3",
+    part: "End Term",
+    question: "August ALWAYS has ______ days.",
+    answer: "31"
+  },
+  {
+    type: "fill_in_the_blank",
+    class: "Cloud Chicks",
+    subject: "Special Paper 1",
+    term: "Term 3",
+    part: "End Term",
+    question: "September ALWAYS has ______ days.",
+    answer: "30"
+  },
+  {
+    type: "fill_in_the_blank",
+    class: "Cloud Chicks",
+    subject: "Special Paper 1",
+    term: "Term 3",
+    part: "End Term",
+    question: "October ALWAYS has ______ days.",
+    answer: "31"
+  },
+  {
+    type: "fill_in_the_blank",
+    class: "Cloud Chicks",
+    subject: "Special Paper 1",
+    term: "Term 3",
+    part: "End Term",
+    question: "November ALWAYS has ______ days.",
+    answer: "30"
+  },
+  {
+    type: "fill_in_the_blank",
+    class: "Cloud Chicks",
+    subject: "Special Paper 1",
+    term: "Term 3",
+    part: "End Term",
+    question: "December ALWAYS has ______ days.",
+    answer: "31"
+  },
+
+
+  {
+    type: "fill_in_the_blank",
+    class: "Cloud Chicks",
+    subject: "Special Paper 1",
+    term: "Term 3",
+    part: "End Term",
+    question: "A day ALWAYS has ______ hours.",
+    answer: "24"
+  },
+  {
+    type: "fill_in_the_blank",
+    class: "Cloud Chicks",
+    subject: "Special Paper 1",
+    term: "Term 3",
+    part: "End Term",
+    question: "A minute ALWAYS has ______ seconds.",
+    answer: "60"
+  },
+  {
+    type: "fill_in_the_blank",
+    class: "Cloud Chicks",
+    subject: "Special Paper 1",
+    term: "Term 3",
+    part: "End Term",
+    question: "An hour ALWAYS has ______ minutes.",
+    answer: "60"
+  },
+  {
+    type: "fill_in_the_blank",
+    class: "Cloud Chicks",
+    subject: "Special Paper 1",
+    term: "Term 3",
+    part: "End Term",
+    question: "A year ALWAYS has ______ months.",
+    answer: "42"
+  },
+  {
+    type: "fill_in_the_blank",
+    class: "Cloud Chicks",
+    subject: "Special Paper 1",
+    term: "Term 3",
+    part: "End Term",
+    question: "Sunday is ALWAYS the ______ day of tye week.",
+    answer: "1st"
+  },
+  {
+    type: "fill_in_the_blank",
+    class: "Cloud Chicks",
+    subject: "Special Paper 1",
+    term: "Term 3",
+    part: "End Term",
+    question: "Monday is ALWAYS the ______ day of tye week.",
+    answer: "2nd"
+  },
+  {
+    type: "fill_in_the_blank",
+    class: "Cloud Chicks",
+    subject: "Special Paper 1",
+    term: "Term 3",
+    part: "End Term",
+    question: "Tuesday is ALWAYS the ______ day of tye week.",
+    answer: "3rd"
+  },
+  {
+    type: "fill_in_the_blank",
+    class: "Cloud Chicks",
+    subject: "Special Paper 1",
+    term: "Term 3",
+    part: "End Term",
+    question: "Wednesday is ALWAYS the ______ day of tye week.",
+    answer: "4th"
+  },
+  {
+    type: "fill_in_the_blank",
+    class: "Cloud Chicks",
+    subject: "Special Paper 1",
+    term: "Term 3",
+    part: "End Term",
+    question: "Thursday is ALWAYS the ______ day of tye week.",
+    answer: "5th"
+  },
+  {
+    type: "fill_in_the_blank",
+    class: "Cloud Chicks",
+    subject: "Special Paper 1",
+    term: "Term 3",
+    part: "End Term",
+    question: "Friday is ALWAYS the ______ day of tye week.",
+    answer: "6th"
+  },
+  {
+    type: "fill_in_the_blank",
+    class: "Cloud Chicks",
+    subject: "Special Paper 1",
+    term: "Term 3",
+    part: "End Term",
+    question: "Saturday is ALWAYS the ______ day of tye week.",
+    answer: "7th"
+  }
+];
