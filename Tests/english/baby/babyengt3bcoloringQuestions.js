8c79dc6cdfb751daa3c6c33fc1e9cb33384d64b1
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2024-11-08 11:41:20 +0200

    Add files via upload

---
+++ Tests/english/baby/babyengt3bcoloringQuestions.js
@@ -0,0 +1,82 @@
+const babyengt3bcoloringQuestions = [
+{
+    type: "coloring",
+    class: "Baby Birds",
+    subject: "English",
+    term: "Term 3",
+    part: "End Term",
+    coloringType: "letter",
+    question: "Color the letter W",
+    coloringContent: "W"
+},
+{
+    type: "coloring",
+    class: "Baby Birds",
+    subject: "English",
+    term: "Term 3",
+    part: "End Term",
+    coloringType: "letter",
+    question: "Color the letter w",
+    coloringContent: "w"
+},
+{
+    type: "coloring",
+    class: "Baby Birds",
+    subject: "English",
+    term: "Term 3",
+    part: "End Term",
+    coloringType: "letter",
+    question: "Color the letter X",
+    coloringContent: "X"
+},
+{
+    type: "coloring",
+    class: "Baby Birds",
+    subject: "English",
+    term: "Term 3",
+    part: "End Term",
+    coloringType: "letter",
+    question: "Color the letter x",
+    coloringContent: "x"
+},
+{
+    type: "coloring",
+    class: "Baby Birds",
+    subject: "English",
+    term: "Term 3",
+    part: "End Term",
+    coloringType: "letter",
+    question: "Color the letter Y",
+    coloringContent: "Y"
+},
+{
+    type: "coloring",
+    class: "Baby Birds",
+    subject: "English",
+    term: "Term 3",
+    part: "End Term",
+    coloringType: "letter",
+    question: "Color the letter y",
+    coloringContent: "y"
+},
+{
+    type: "coloring",
+    class: "Baby Birds",
+    subject: "English",
+    term: "Term 3",
+    part: "End Term",
+    coloringType: "letter",
+    question: "Color the letter Z",
+    coloringContent: "Z"
+},
+{
+    type: "coloring",
+    class: "Baby Birds",
+    subject: "English",
+    term: "Term 3",
+    part: "End Term",
+    coloringType: "letter",
+    question: "Color the letter z",
+    coloringContent: "z"
+}
+];
---
+++ Tests/english/baby/babyengt3bmatchingQuestions.js
@@ -0,0 +1,15 @@
+const midengt3bmatchingQuestions = [
+  {
+    type: "matching",
+    class: "Sky Sprouts",
+    subject: "English",
+    term: "Term 3",
+    part: "End Term",
+    question: "Match the sound with the word the begins with it:",
+    pairs: [
+      { question: "Ww", answer: "Whale" },
+      { question: "Xx", answer: "Xylophone" },
+      { question: "Yy", answer: "Yatch" }
+    ]
+  },
+]
