1c48935f35d03160fa07809c184b714e4e81f59a
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2024-11-11 12:44:20 +0200

    Add files via upload

--- Tests/math/grade1/g1matht3bprimaryQuestions.js
+++ Tests/math/grade1/g1matht3bprimaryQuestions.js
@@ -1,19 +1,16 @@
 const g1matht3bprimaryQuestions = [
-{
-    type: "math",
-    subtype: "primary",
-    class: "Feather Flyers",
-    subject: "Mathematics",
-    term: "Term 3",
-    part: "End Term",
-    question: "If a pattern goes red, blue, red, blue, what comes next?",
-    mathContent: [
-    "3 + 2 =", 
-    "7 - 4 =", 
-    "5 + 5 =", 
-    "10 - 3 ="
-    ]
-},
-
-
+    {
+        type: "math",
+        subtype: "simpleArithmetic",
+        class: "Feather Flyers",
+        subject: "Mathematics",
+        term: "Term 3",
+        part: "End Term",
+        question: "Solve the Following",
+        mathPrompt: "Addition",
+        num1: 7,
+        num2: 5,
+        operator: "+",
+    }
+    // Add more questions as needed
 ];
