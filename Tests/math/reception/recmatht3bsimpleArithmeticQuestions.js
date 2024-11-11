716a1503f482ea2607619f3b737bcf33b4c058cd
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2024-11-11 12:45:24 +0200

    Add files via upload

---
+++ Tests/math/reception/recmatht3bsimpleArithmeticQuestions.js
@@ -0,0 +1,15 @@
+const recmatht3bsimpleArithmeticQuestions = [
+    {
+        type: "math",
+        subtype: "simpleArithmetic",
+        class: "Nestlings Navigators",
+        subject: "Mathematics",
+        term: "Term 3",
+        part: "End Term",
+        question: "Solve the Following",
+        num1: 7,
+        num2: 5,
+        operator: "+",
+    }
+    // Add more questions as needed
+];
