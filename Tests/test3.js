70a519d4ff82291d29de73ea42a9a332c8f81c64
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2024-11-19 09:20:16 +0200

    Add files via upload

--- Tests/test3.js
+++ Tests/test3.js
@@ -126,17 +126,7 @@ document.getElementById('savePDF').addEventListener('click', () => {
     classImages.style.display = 'block';
 });
 
-// Function to generate a filename with a timestamp
-function generateFilename() {
-    const now = new Date();
-    const year = now.getFullYear();
-    const month = (now.getMonth() + 1).toString().padStart(2, '0');
-    const day = now.getDate().toString().padStart(2, '0');
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds().toString().padStart(2, '0');
-    return `document_${year}${month}${day}_${hours}${minutes}${seconds}`;
-}
+
 
 
 
