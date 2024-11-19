3b3b70ec7d2a2543386f3b321ef2342062e3b9a0
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2024-11-19 22:51:47 +0200

    Add files via upload

--- Tests/sp1/grade2/g2sp1t3bwordSearchQuestions.js
+++ Tests/sp1/grade2/g2sp1t3bwordSearchQuestions.js
@@ -8,7 +8,7 @@ const g2sp1t3bwordSearchQuestions = [
     "question": "Find the words related to the properties of sound hidden in the puzzle.",
     "puzzlePrompt": "10x10 grid",
     "targetWords": ["VIBRATION", "PITCH", "VOLUME", "FREQUENCY", "AMPLITUDE", "SOUND", "MUSIC", "TUNING", "NOISE", "EARS"],
-    "gridSize": 10
+    "gridSize": 15
 },
 {
     "type": "wordSearchPuzzle",
@@ -19,7 +19,7 @@ const g2sp1t3bwordSearchQuestions = [
     "question": "Find the words related to sound production hidden in the puzzle.",
     "puzzlePrompt": "10x10 grid",
     "targetWords": ["SOUND", "VIBRATE", "WAVES", "MEDIUM", "PITCH", "VOLUME", "INSTRUMENT", "VOICE", "MACHINE", "TENSION"],
-    "gridSize": 10
+    "gridSize": 15
 },
 {
     "type": "wordSearchPuzzle",
@@ -30,7 +30,7 @@ const g2sp1t3bwordSearchQuestions = [
     "question": "Find the words related to sound and hearing hidden in the puzzle.",
     "puzzlePrompt": "10x10 grid",
     "targetWords": ["SOUND", "EAR", "VIBRATIONS", "HEARING", "OUTER", "MIDDLE", "INNER", "PITCH", "WAVES", "BRAIN"],
-    "gridSize": 10
+    "gridSize": 15
 },
 {
     "type": "wordSearchPuzzle",
@@ -41,7 +41,7 @@ const g2sp1t3bwordSearchQuestions = [
     "question": "Find the words related to sound in our environment hidden in the puzzle.",
     "puzzlePrompt": "10x10 grid",
     "targetWords": ["SOUND", "NOISE", "MUSIC", "BIRDS", "WIND", "TRAFFIC", "HEARING", "CONTROL", "POLLUTION", "HEALTH"],
-    "gridSize": 10
+    "gridSize": 15
 },
 {
     "type": "wordSearchPuzzle",
@@ -52,7 +52,7 @@ const g2sp1t3bwordSearchQuestions = [
     "question": "Find the words related to Earth and space hidden in the puzzle.",
     "puzzlePrompt": "10x10 grid",
     "targetWords": ["EARTH", "SPACE", "SUN", "PLANET", "STARS", "GRAVITY", "GLOBE", "ATMOSPHERE", "SOLAR", "SYSTEM"],
-    "gridSize": 10
+    "gridSize": 15
 },
 {
     "type": "wordSearchPuzzle",
@@ -63,7 +63,7 @@ const g2sp1t3bwordSearchQuestions = [
     "question": "Find the geographical features hidden in the puzzle.",
     "puzzlePrompt": "8x8 grid",
     "targetWords": ["LAND", "WATER", "MOUNTAIN", "VALLEY", "PLATEAU", "PLAIN", "CANYON", "ISLAND", "OCEAN", "RIVER"],
-    "gridSize": 8
+    "gridSize": 15
 },
 {
     "type": "wordSearchPuzzle",
@@ -74,7 +74,7 @@ const g2sp1t3bwordSearchQuestions = [
     "question": "Find the words related to day and night in the puzzle.",
     "puzzlePrompt": "8x8 grid",
     "targetWords": ["DAY", "NIGHT", "EARTH", "SUN", "AXIS", "ROTATION", "CYCLE", "TILT", "MODEL", "TIME"],
-    "gridSize": 8
+    "gridSize": 15
 },
 {
     "type": "wordSearchPuzzle",
@@ -85,7 +85,7 @@ const g2sp1t3bwordSearchQuestions = [
     "question": "Find the words related to the Sun and solar energy hidden in the puzzle.",
     "puzzlePrompt": "8x8 grid",
     "targetWords": ["SUN", "STAR", "LIGHT", "ENERGY", "SOLAR", "PLANET", "EARTH", "POWER", "DEVICE", "PANELS"],
-    "gridSize": 8
+    "gridSize": 15
 },
 {
     "type": "wordSearchPuzzle",
@@ -96,7 +96,7 @@ const g2sp1t3bwordSearchQuestions = [
     "question": "Find the words related to the Moon and its phases hidden in the puzzle.",
     "puzzlePrompt": "10x10 grid",
     "targetWords": ["MOON", "TIDES", "PHASES", "CRESCENT", "GIBBOUS", "EARTH", "GRAVITY", "NEW", "FULL", "QUARTER"],
-    "gridSize": 10
+    "gridSize": 15
 },
 {
     "type": "wordSearchPuzzle",
@@ -118,7 +118,7 @@ const g2sp1t3bwordSearchQuestions = [
     "question": "Find the words related to the Solar System hidden in the puzzle.",
     "puzzlePrompt": "9x9 grid",
     "targetWords": ["SUN", "PLANET", "MOON", "EARTH", "MARS", "JUPITER", "SATURN", "URANUS", "NEPTUNE", "ORBIT"],
-    "gridSize": 9
+    "gridSize": 15
 },
 {
     "type": "wordSearchPuzzle",
@@ -129,7 +129,7 @@ const g2sp1t3bwordSearchQuestions = [
     "question": "Find the words related to the universe hidden in the puzzle.",
     "puzzlePrompt": "10x10 grid",
     "targetWords": ["UNIVERSE", "GALAXY", "STARS", "PLANETS", "MILKY", "SPACE", "TIME", "MATTER", "ENERGY", "DUST"],
-    "gridSize": 10
+    "gridSize": 15
 },
 {
     "type": "wordSearchPuzzle",
@@ -140,6 +140,6 @@ const g2sp1t3bwordSearchQuestions = [
     "question": "Find the words related to environmental conservation hidden in the puzzle.",
     "puzzlePrompt": "10x10 grid",
     "targetWords": ["CONSERVE", "RECYCLING", "POLLUTION", "WILDLIFE", "RESOURCES", "SORTING", "RECYCLE", "ENVIRONMENT", "LANDFILL", "PROTECT"],
-    "gridSize": 10
+    "gridSize": 15
 }
 ];
