2eb412f7a6b3ccece1c44e6080f24880a7d1c2a9
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2024-11-19 22:51:06 +0200

    Add files via upload

--- Tests/sp1/grade1/g1sp1t3bwordSearchQuestions.js
+++ Tests/sp1/grade1/g1sp1t3bwordSearchQuestions.js
@@ -8,7 +8,7 @@ const g1sp1t3bwordSearchQuestions = [
     "question": "Find the words related to force hidden in the puzzle.",
     "puzzlePrompt": "10x10 grid",
     "targetWords": ["FORCE", "PUSH", "PULL", "NEWTON", "POUND", "SCALE", "OBJECT", "SPRING", "MOVE", "DIRECTION"],
-    "gridSize": 10
+    "gridSize": 15
 },
 {
     "type": "wordSearchPuzzle",
@@ -19,7 +19,7 @@ const g1sp1t3bwordSearchQuestions = [
     "question": "Find the words related to forces hidden in the puzzle.",
     "puzzlePrompt": "10x10 grid",
     "targetWords": ["FORCE", "PUSH", "PULL", "MOTION", "BALANCED", "UNBALANCED", "OBJECT", "SPEED", "DIRECTION", "FRICTION"],
-    "gridSize": 10
+    "gridSize": 15
 },
 {
     "type": "wordSearchPuzzle",
@@ -30,7 +30,7 @@ const g1sp1t3bwordSearchQuestions = [
     "question": "Find the words related to friction hidden in the puzzle.",
     "puzzlePrompt": "10x10 grid",
     "targetWords": ["FRICTION", "FORCE", "MOVEMENT", "SURFACE", "ROUGH", "SLIDE", "STOP", "WALK", "GRAVEL", "TIRES"],
-    "gridSize": 10
+    "gridSize": 15
 },
 {
     "type": "wordSearchPuzzle",
@@ -41,7 +41,7 @@ const g1sp1t3bwordSearchQuestions = [
     "question": "Find the words related to forces hidden in the puzzle.",
     "puzzlePrompt": "6x6 grid",
     "targetWords": ["FORCE", "PUSH", "PULL", "MOTION", "LEVER", "PULLEY", "BRAKE", "PEDAL", "KICK", "MOVE"],
-    "gridSize": 6
+    "gridSize": 10
 },
 {
     "type": "wordSearchPuzzle",
@@ -52,7 +52,7 @@ const g1sp1t3bwordSearchQuestions = [
     "question": "Find the words related to magnetism hidden in the puzzle.",
     "puzzlePrompt": "10x10 grid",
     "targetWords": ["MAGNET", "IRON", "NORTH", "SOUTH", "POLES", "FIELD", "FORCE", "COMPASS", "REPEL", "ATTRACT"],
-    "gridSize": 10
+    "gridSize": 15
 },
 {
     "type": "wordSearchPuzzle",
@@ -63,7 +63,7 @@ const g1sp1t3bwordSearchQuestions = [
     "question": "Find the terms related to forces hidden in the puzzle.",
     "puzzlePrompt": "10x10 grid",
     "targetWords": ["FORCE", "PUSH", "PULL", "FRICTION", "GRAVITY", "TENSION", "MOVEMENT", "ACCELERATION", "DEFORMATION", "NEWTON"],
-    "gridSize": 10
+    "gridSize": 15
 },
 {
     "type": "wordSearchPuzzle",
@@ -74,7 +74,7 @@ const g1sp1t3bwordSearchQuestions = [
     "question": "Find the words related to health and growth hidden in the puzzle.",
     "puzzlePrompt": "10x10 grid",
     "targetWords": ["HEALTH", "GROWTH", "WELLNESS", "EXERCISE", "NUTRITION", "MENTAL", "INFANCY", "CHILDHOOD", "ADOLESCENCE", "ADULTHOOD"],
-    "gridSize": 10
+    "gridSize": 15
 },
 {
     "type": "wordSearchPuzzle",
@@ -85,7 +85,7 @@ const g1sp1t3bwordSearchQuestions = [
     "question": "Find the words related to basic health needs and healthy lifestyle hidden in the puzzle.",
     "puzzlePrompt": "10x10 grid",
     "targetWords": ["HEALTH", "EXERCISE", "NUTRIENTS", "VITAMINS", "SLEEP", "PROTEINS", "FIBER", "STRENGTH", "FRUITS", "ENERGY"],
-    "gridSize": 10
+    "gridSize": 15
 },
 {
     "type": "wordSearchPuzzle",
@@ -96,7 +96,7 @@ const g1sp1t3bwordSearchQuestions = [
     "question": "Find the words related to personal hygiene hidden in the puzzle.",
     "puzzlePrompt": "9x9 grid",
     "targetWords": ["HYGIENE", "CLEAN", "HEALTH", "BATHING", "GERMS", "WASHING", "BRUSHING", "ILLNESS", "CONFIDENCE", "IMMUNE"],
-    "gridSize": 9
+    "gridSize": 15
 },
 {
     "type": "wordSearchPuzzle",
@@ -107,7 +107,7 @@ const g1sp1t3bwordSearchQuestions = [
     "question": "Find the words related to growth and change hidden in the puzzle.",
     "puzzlePrompt": "10x10 grid",
     "targetWords": ["GROWTH", "CHANGE", "NUTRITION", "PHYSICAL", "EMOTIONAL", "COGNITIVE", "LIFE", "INFANCY", "ADOLESCENCE", "AGING"],
-    "gridSize": 10
+    "gridSize": 15
 },
 {
     "type": "wordSearchPuzzle",
@@ -118,7 +118,7 @@ const g1sp1t3bwordSearchQuestions = [
     "question": "Find the words related to healthy habits and safety rules hidden in the puzzle.",
     "puzzlePrompt": "10x10 grid",
     "targetWords": ["HEALTHY", "SLEEP", "EXERCISE", "SAFETY", "ACCIDENT", "AWARENESS", "CROSSING", "HABITS", "ROADS", "RULES"],
-    "gridSize": 10
+    "gridSize": 15
 },
 {
     "type": "wordSearchPuzzle",
@@ -129,7 +129,7 @@ const g1sp1t3bwordSearchQuestions = [
     "question": "Find the words related to good and bad habits hidden in the puzzle.",
     "puzzlePrompt": "10x10 grid",
     "targetWords": ["HABITS", "HEALTHY", "EXERCISE", "FOODS", "SNACKS", "SCREEN", "OBESITY", "DENTAL", "WELLBEING", "ACTIVITY"],
-    "gridSize": 10
+    "gridSize": 15
 },
 {
     "type": "wordSearchPuzzle",
@@ -140,7 +140,7 @@ const g1sp1t3bwordSearchQuestions = [
     "question": "Find the key terms related to healthcare professionals and check-ups hidden in the puzzle.",
     "puzzlePrompt": "10x10 grid",
     "targetWords": ["HEALTH", "DOCTORS", "NURSES", "CHECKUP", "DIAGNOSIS", "TREATMENT", "CARE", "VITALS", "EDUCATION", "PREVENTIVE"],
-    "gridSize": 10
+    "gridSize": 15
 },
 {
     "type": "wordSearchPuzzle",
@@ -151,6 +151,6 @@ const g1sp1t3bwordSearchQuestions = [
     "question": "Find the key concepts related to health and growth hidden in the puzzle.",
     "puzzlePrompt": "10x10 grid",
     "targetWords": ["HEALTH", "GROWTH", "NUTRITION", "EXERCISE", "HABITS", "DIET", "VITAMINS", "MINERALS", "CHILDHOOD", "ADULTHOOD"],
-    "gridSize": 10
+    "gridSize": 15
 }
 ];
