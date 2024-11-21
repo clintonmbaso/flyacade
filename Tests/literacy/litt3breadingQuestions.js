ab2ff6b97730096e90d712dc611ec84c5bf36639
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2024-11-21 10:06:29 +0200

    Add files via upload

---
+++ Tests/literacy/litt3breadingQuestions.js
@@ -0,0 +1,262 @@
+const litt3breadingQuestions = [
+{
+     type: "reading",
+     subtype: "words",
+     class: "Nestlings Navigators",
+     subject: "Literacy",
+     term: "Term 3",
+     part: "End Term",
+    question: "Read the following CVC words starting with vowel 'u'.",
+    readingContent: [
+            "Bug",
+            "Rug",
+            "Mug",
+            "Dug",
+            "Hug"
+        ]
+},
+{
+  type: "reading",
+     subtype: "words",
+     class: "Nestlings Navigators",
+     subject: "Literacy",
+     term: "Term 3",
+     part: "End Term",  
+    question: "Read the following CVC words with vowel 'u'.",
+    readingContent: [
+            "Cub",
+            "Tub",
+            "Rub",
+            "Fun",
+            "Run"
+        ]
+},
+{
+  type: "reading",
+     subtype: "words",
+     class: "Nestlings Navigators",
+     subject: "Literacy",
+     term: "Term 3",
+     part: "End Term",  
+    question: "Read the following CVC words with vowel 'u'.",
+    readingContent: [
+            "Sun",
+            "Bun",
+            "Gum",
+            "Cut",
+            "Hut"
+        ]
+},
+{
+  type: "reading",
+     subtype: "words",
+     class: "Nestlings Navigators",
+     subject: "Literacy",
+     term: "Term 3",
+     part: "End Term",  
+    question: "Read the following CVC words with vowel 'u'.",
+    readingContent: [
+            "Nut",
+            "Pup",
+            "Mud",
+            "Cup",
+            "Bud"
+        ]
+},
+{
+     type: "reading",
+     subtype: "short-sentences",
+     class: "Feather Flyers",
+     subject: "Literacy",
+     term: "Term 3",
+     part: "End Term",
+    question: "Read the following sentences.",
+    readingContent: [
+     "The cat sat on the mat.",
+     "Dad has a red hat.",
+     "The sun is hot.",
+     "I see a big bug.",
+     "The dog can run fast."
+        ]
+},
+{
+  type: "reading",
+     subtype: "short-sentences",
+     class: "Feather Flyers",
+     subject: "Literacy",
+     term: "Term 3",
+     part: "End Term",  
+    question: "Read the following sentences.",
+    readingContent: [
+     "The cub is fun.",
+     "I see a red top.",
+     "The cat has a cap.",
+     "The man is big.",
+     "The jet can go fast."
+        ]
+},
+{
+  type: "reading",
+     subtype: "short-sentences",
+     class: "Feather Flyers",
+     subject: "Literacy",
+     term: "Term 3",
+     part: "End Term",  
+    question: "Read the following sentences.",
+    readingContent: [
+     "The pen is wet.",
+     "The kid can hop.",
+     "The fan is on the van.",
+     "She has a pet hen.",
+     "He cut the box."
+        ]
+},
+{
+  type: "reading",
+     subtype: "short-sentences",
+     class: "Feather Flyers",
+     subject: "Literacy",
+     term: "Term 3",
+     part: "End Term",  
+    question: "Read the following sentences.",
+    readingContent: [
+     "Mom has a red cup.",
+     "The bat is in the bag.",
+     "The pig is in the mud.",
+     "He sat on the log.",
+     "I see a web on the bed."
+        ]
+},
+
+
+{
+     type: "reading",
+     subtype: "short-sentences",
+     class: "Feather Flyers",
+     subject: "Literacy",
+     term: "Term 3",
+     part: "End Term",
+    question: "Read the following sentences.",
+    readingContent: [
+     "The cat is on the mat.",
+     "I see a big red hat.",
+     "The dog can run on a mat.",
+     "She has a pet hen.",
+     "The sun is hot."
+        ]
+},
+{
+  type: "reading",
+     subtype: "short-sentences",
+     class: "Cloud Chicks",
+     subject: "Literacy",
+     term: "Term 3",
+     part: "End Term",  
+    question: "Read the following sentences.",
+    readingContent: [
+     "He sat on the log.",
+     "The man has a cup.",
+     "Ben has a big bed.",
+     "I see a bug on the rug.",
+     "The pig is in the mud."
+        ]
+},
+{
+  type: "reading",
+     subtype: "short-sentences",
+     class: "Cloud Chicks",
+     subject: "Literacy",
+     term: "Term 3",
+     part: "End Term",  
+    question: "Read the following sentences.",
+    readingContent: [
+            "The kid has a map.",
+            "We can sit on the top step.",
+            "The cat will nap on the mat.",
+            "I see a ten in the pen.",
+            "Mom and dad sit on the bed."
+        ]
+},
+{
+  type: "reading",
+     subtype: "short-sentences",
+     class: "Cloud Chicks",
+     subject: "Literacy",
+     term: "Term 3",
+     part: "End Term",  
+    question: "Read the following sentences.",
+    readingContent: [
+            "She will cut the pin.",
+            "The bat is in the bag.",
+            "He can hop on one leg.",
+            "The cup is red and big.",
+            "I see a web in the hut."
+        ]
+},
+
+
+{
+     type: "reading",
+     subtype: "short-sentences",
+     class: "Wing Whiz",
+     subject: "Literacy",
+     term: "Term 3",
+     part: "End Term",
+    question: "Read the following sentences.",
+    readingContent: [
+     "The cat with the big hat sat on the soft mat.",
+     "A red hen sat on the wet bed to stay warm.",
+     "The dog likes to run fast in the big sun.",
+     "I have a pet pig that loves to nap in the mud.",
+     "The sun is hot, so we sit under a big tree."
+        ]
+},
+{
+  type: "reading",
+     subtype: "short-sentences",
+     class: "Wing Whiz",
+     subject: "Literacy",
+     term: "Term 3",
+     part: "End Term",  
+    question: "Read the following sentences.",
+    readingContent: [
+     "I see a bug crawling on a brown log.",
+     "My dad made a fun hut for us in the backyard.",
+     "A little pup was hiding under the bed all night.",
+     "The kitten will sip milk from a small dish.",
+     "The big frog likes to hop near the wet pond."
+        ]
+},
+{
+  type: "reading",
+     subtype: "short-sentences",
+     class: "Sky Soarers",
+     subject: "Literacy",
+     term: "Term 3",
+     part: "End Term",  
+    question: "Read the following sentences.",
+    readingContent: [
+     "The kid and his pet dog sat near the muddy lake.",
+     "I see a big red van next to the tiny cab.",
+     "The cat will nap in her warm bed after she eats.",
+     "A fat pig rolled in the mud and got dirty.",
+     "The fox with the red hat ran up the big hill."
+        ]
+},
+{
+  type: "reading",
+     subtype: "short-sentences",
+     class: "Sky Soarers",
+     subject: "Literacy",
+     term: "Term 3",
+     part: "End Term",  
+    question: "Read the following sentences.",
+    readingContent: [
+     "My dad found a big bug under the green rug.",
+     "The cat and the dog are friends and play on the big log.",
+     "My mom made a red hat for my little pet.",
+     "The man with a blue cap has a nice, warm cup of tea.",
+     "The big bat was hiding inside the dark bag."
+        ]
+}
+];
