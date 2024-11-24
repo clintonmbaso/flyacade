c34f176b159ba409f2b6b0a110ba7dca7d118e91
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2024-11-25 00:15:35 +0200

    Add files via upload

---
+++ Tests/ss/sst3bimagingQuestions.js
@@ -0,0 +1,82 @@
+const sst3bimagingQuestions = [
+{
+    type: "imaging",
+    class: "Baby Birds",
+    subject: "Social Studies",
+    term: "Term 3",
+    part: "End Term",
+    question: "Carefully colour the image given below.",
+    imageSrc: "./images/social_studies/emptyZambia.jpg",
+    questions: [
+        {
+            description: "Map of Zambia",
+            subType: "fill_in_the_blank",
+        }
+    ]
+},
+{
+    type: "imaging",
+    class: "Sky Sprouts",
+    subject: "Social Studies",
+    term: "Term 3",
+    part: "End Term",
+    question: "Carefully trace the image given below.",
+    imageSrc: "./images/social_studies/emptyZambia.jpg",
+    questions: [
+        {
+            description: "Map of Zambia",
+            subType: "fill_in_the_blank",
+        },
+    ]
+},
+/*
+{
+    type: "imaging",
+    class: "Nestlings Navigators",
+    subject: "Social Studies",
+    term: "Term 3",
+    part: "End Term",
+    question: "Carefully look at the image given below and answer the questions that follow.",
+    imageSrc: "./images/social_studies/namesZambia.jpg",
+    questions: [
+        {
+            description: "Write down a name on the Map that begins with letter M",
+            subType: "fill_in_the_blank",
+            answersCount: 3
+        },
+        {
+            description: "Write down a name on the Map that begins with letter T",
+            subType: "fill_in_the_blank",
+        },
+        {
+            description: "Write down a name on the Map that begins with letter S",
+            subType: "fill_in_the_blank",
+        },
+        {
+            description: "Write down a name on the Map that begins with letter L",
+            subType: "fill_in_the_blank",
+        },
+        {
+            description: "Write down a name on the Map that begins with letter C",
+            subType: "fill_in_the_blank",
+        }
+    ]
+},
+*/
+{
+    type: "imaging",
+    class: "Nestlings Navigators",
+    subject: "Social Studies",
+    term: "Term 3",
+    part: "End Term",
+    question: "Carefully look at the image given below and answer the questions that follow.",
+    imageSrc: "./images/social_studies/namesZambia.jpg",
+    questions: [
+        {
+            description: "Write down a name on the Map that begins with letter M",
+            subType: "fill_in_the_blank",
+             // Three input fields will appear for this question
+        }
+    ]
+}
+];
