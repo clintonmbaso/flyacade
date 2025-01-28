831cf867dab189cf49ed3d840b6f1e25b65410bc
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2025-01-28 06:02:44 +0200

    Add files via upload

---
+++ Lessons/Social_Studies/g6ssdata3.js
@@ -0,0 +1,63 @@
+const lessonPlans36c = [
+
+// Week 2
+// Lesson 1
+
+// Lesson 2
+
+// Lesson 3
+
+// Lesson 4
+
+// Lesson 5
+
+// Week 3
+// Lesson 1
+
+// Lesson 2
+
+// Lesson 3
+
+// Week 4
+// Lesson 1
+
+// Lesson 2
+
+// Lesson 3
+
+// Week 5
+// Lesson 1
+
+// Lesson 2
+
+// Lesson 3
+
+// Week 7
+// Lesson 1
+
+// Lesson 2
+
+// Lesson 3
+
+// Week 8
+// Lesson 1
+
+// Lesson 2
+
+// Lesson 3
+
+// Week 9
+// Lesson 1
+
+// Lesson 2
+
+// Lesson 3
+
+// Week 10
+// Lesson 1
+
+// Lesson 2
+
+// Lesson 3
+
+];
