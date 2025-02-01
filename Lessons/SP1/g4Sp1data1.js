808246724ef50af934f342b76539db4ba42ce506
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2025-02-01 20:21:09 +0200

    Update g4Sp1data1.js

--- Lessons/SP1/g4Sp1data1.js
+++ Lessons/SP1/g4Sp1data1.js
@@ -1,30 +1,43 @@
 const lessonPlans64a = [
-// Week 2
+
+// Week 2
 // Lesson 1
 
-// Week 3
+// Lesson 2
+
+// Week 3
 // Lesson 1
 
-// Week 4
+// Lesson 2
+
+// Week 4
 // Lesson 1
 
-// Week 5
+// Lesson 2
+
+// Week 5
 // Lesson 1
 
+// Lesson 2
 
-// Week 7
+// Week 7
 // Lesson 1
 
-// Week 8
+// Lesson 2
+
+// Week 8
 // Lesson 1
 
-// Week 9
+// Lesson 2
+
+// Week 9
 // Lesson 1
 
-// Week 10
+// Lesson 2
+
+// Week 10
 // Lesson 1
 
-	
-	
-	
+// Lesson 2
+
 ];
