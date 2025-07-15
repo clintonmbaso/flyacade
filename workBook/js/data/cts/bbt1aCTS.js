f664501b17f267cfb2050f888631c7cc0d40a1e5
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2025-07-15 06:40:13 +0200

    Add files via upload

---
+++ workBook/js/data/cts/bbt1aCTS.js
@@ -0,0 +1,113 @@
+window.registerWorkbook({
+    id: 'bbt1aCTS',
+    title: 'Baby Birds Adventures',
+    description: 'Fun learning activities for our youngest learners',
+    subject: 'Creative and Technology Studies',
+    grade: 'baby',
+    term: 1,
+    pages: [
+        {
+            id: 'colors-and-shapes',
+            title: 'Colors and Shapes',
+            questionLimit: 3, // Show all 10 questions
+            exercises: [
+                {
+                    id: 'color-recognition',
+                    title: 'Color Recognition',
+                    questions: [
+                        {
+                            type: 'coloring',
+                            item: 'apple',
+                            image: 'images/fruits/apple-outline.png',
+                            colors: ['#ff0000', '#00ff00', '#0000ff', '#ffff00'],
+                            correctColor: '#ff0000',
+                            correctColorName: 'Red',
+                            explanation: "Great job! Apples are usually red. Some can be green or yellow too!"
+                        },
+                        {
+                            type: 'coloring',
+                            item: 'banana',
+                            image: 'images/fruits/banana-outline.png',
+                            colors: ['#ffff00', '#00ff00', '#ffa500', '#ffffff'],
+                            correctColor: '#ffff00',
+                            correctColorName: 'Yellow',
+                            explanation: "Perfect! Bananas turn yellow when they're ripe and ready to eat!"
+                        },
+                        {
+                            type: 'coloring',
+                            item: 'tree leaves',
+                            image: 'images/nature/tree-outline.png',
+                            colors: ['#00ff00', '#a52a2a', '#0000ff', '#ffff00'],
+                            correctColor: '#00ff00',
+                            correctColorName: 'Green',
+                            explanation: "Excellent! Leaves are green because of chlorophyll that helps plants make food."
+                        },
+                        {
+                            type: 'coloring',
+                            item: 'orange fruit',
+                            image: 'images/fruits/orange-outline.png',
+                            colors: ['#ffa500', '#ff0000', '#ffff00', '#00ff00'],
+                            correctColor: '#ffa500',
+                            correctColorName: 'Orange',
+                            explanation: "Wonderful! Oranges are named after their beautiful orange color!"
+                        },
+                        {
+                            type: 'coloring',
+                            item: 'grape',
+                            image: 'images/fruits/grape-outline.png',
+                            colors: ['#800080', '#00ff00', '#0000ff', '#ff0000'],
+                            correctColor: '#800080',
+                            correctColorName: 'Purple',
+                            explanation: "Awesome! Many grapes are purple, but some are green like the ones we eat too!"
+                        },
+                        {
+                            type: 'coloring',
+                            item: 'carrot',
+                            image: 'images/vegetables/carrot-outline.png',
+                            colors: ['#ffa500', '#ff0000', '#00ff00', '#ffff00'],
+                            correctColor: '#ffa500',
+                            correctColorName: 'Orange',
+                            explanation: "Yummy! Carrots are orange because they contain beta-carotene which is good for our eyes!"
+                        },
+                        {
+                            type: 'coloring',
+                            item: 'strawberry',
+                            image: 'images/fruits/strawberry-outline.png',
+                            colors: ['#ff0000', '#ffc0cb', '#00ff00', '#800080'],
+                            correctColor: '#ff0000',
+                            correctColorName: 'Red',
+                            explanation: "Delicious! Strawberries are bright red with tiny yellow seeds on the outside."
+                        },
+                        {
+                            type: 'coloring',
+                            item: 'blueberry',
+                            image: 'images/fruits/blueberry-outline.png',
+                            colors: ['#0000ff', '#800080', '#ff0000', '#00ff00'],
+                            correctColor: '#0000ff',
+                            correctColorName: 'Blue',
+                            explanation: "Tasty! Blueberries are deep blue with a silvery coating called bloom."
+                        },
+                        {
+                            type: 'coloring',
+                            item: 'sun',
+                            image: 'images/nature/sun-outline.png',
+                            colors: ['#ffff00', '#ffa500', '#ff0000', '#ffffff'],
+                            correctColor: '#ffff00',
+                            correctColorName: 'Yellow',
+                            explanation: "Beautiful! The sun gives us yellow light that makes daytime bright and warm."
+                        },
+                        {
+                            type: 'coloring',
+                            item: 'tree trunk',
+                            image: 'images/nature/tree-trunk-outline.png',
+                            colors: ['#a52a2a', '#00ff00', '#000000', '#ffff00'],
+                            correctColor: '#a52a2a',
+                            correctColorName: 'Brown',
+                            explanation: "Good job! Tree trunks are brown because of their protective bark covering."
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+});
