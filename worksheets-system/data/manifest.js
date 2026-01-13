7469243294e5f37e0e822319b27ffbf0a96ca68c
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2026-01-13 09:55:00 +0200

    Add files via upload

---
+++ worksheets-system/data/manifest.js
@@ -0,0 +1,193 @@
+// data/manifest.js
+// This file lists all available worksheet files without loading them
+const WorksheetManifest = {
+    files: [
+        // Mathematics
+        // Baby
+    /*
+        'data/baby/baby-math-week02.js',
+        'data/baby/baby-math-week03.js',
+        'data/baby/baby-math-week04.js',
+        'data/baby/baby-math-week05.js',
+        'data/baby/baby-math-week07.js',
+        'data/baby/baby-math-week08.js',
+        'data/baby/baby-math-week09.js',
+        'data/baby/baby-math-week10.js',
+        */
+        // Middle
+        'data/middle/middle-math-week02.js',
+        'data/middle/middle-math-week03.js',
+        'data/middle/middle-math-week04.js',
+        'data/middle/middle-math-week05.js',
+        'data/middle/middle-math-week07.js',
+        'data/middle/middle-math-week08.js',
+        'data/middle/middle-math-week09.js',
+        'data/middle/middle-math-week10.js',
+        
+        // Reception
+        'data/reception/reception-math-week02.js',
+        'data/reception/reception-math-week03.js',
+        'data/reception/reception-math-week04.js',
+        'data/reception/reception-math-week05.js',
+        'data/reception/reception-math-week07.js',
+        'data/reception/reception-math-week08.js',
+        'data/reception/reception-math-week09.js',
+        'data/reception/reception-math-week10.js',
+        
+        // Grade 1
+        'data/grade1/g1math-week02.js',
+        'data/grade1/g1math-week03.js',
+        'data/grade1/g1math-week04.js',
+        'data/grade1/g1math-week05.js',
+        'data/grade1/g1math-week07.js',
+        'data/grade1/g1math-week08.js',
+        'data/grade1/g1math-week09.js',
+        'data/grade1/g1math-week10.js',
+        
+        // Grade 2
+        'data/grade2/g2math-week02.js',
+        'data/grade2/g2math-week03.js',
+        'data/grade2/g2math-week04.js',
+        'data/grade2/g2math-week05.js',
+        'data/grade2/g2math-week07.js',
+        'data/grade2/g2math-week08.js',
+        'data/grade2/g2math-week09.js',
+        'data/grade2/g2math-week10.js',
+        
+        // Grade 3
+        'data/grade3/g3math-week02.js',
+        'data/grade3/g3math-week03.js',
+        'data/grade3/g3math-week04.js',
+        'data/grade3/g3math-week05.js',
+        'data/grade3/g3math-week07.js',
+        'data/grade3/g3math-week08.js',
+        'data/grade3/g3math-week09.js',
+        'data/grade3/g3math-week10.js',
+            
+        // Grade 4
+        'data/grade4/g4math-week02.js',
+        'data/grade4/g4math-week03.js',
+        'data/grade4/g4math-week04.js',
+        'data/grade4/g4math-week05.js',
+        'data/grade4/g4math-week07.js',
+        'data/grade4/g4math-week08.js',
+        'data/grade4/g4math-week09.js',
+        'data/grade4/g4math-week10.js',
+                
+         // Grade 5
+        'data/grade5/g5math-week02.js',
+        'data/grade5/g5math-week03.js',
+        'data/grade5/g5math-week04.js',
+        'data/grade5/g5math-week05.js',
+        'data/grade5/g5math-week07.js',
+        'data/grade5/g5math-week08.js',
+        'data/grade5/g5math-week09.js',
+        'data/grade5/g5math-week10.js',
+                   
+    // Science
+        // Baby
+        'data/baby/baby-science-week02.js',
+        'data/baby/baby-science-week03.js',
+        'data/baby/baby-science-week04.js',
+        'data/baby/baby-science-week05.js',
+        'data/baby/baby-science-week07.js',
+        'data/baby/baby-science-week08.js',
+        'data/baby/baby-science-week09.js',
+        'data/baby/baby-science-week10.js',
+            
+        // Middle
+        'data/middle/middle-science-week02.js',
+        'data/middle/middle-science-week03.js',
+        'data/middle/middle-science-week04.js',
+        'data/middle/middle-science-week05.js',
+        'data/middle/middle-science-week07.js',
+        'data/middle/middle-science-week08.js',
+        'data/middle/middle-science-week09.js',
+        'data/middle/middle-science-week10.js',
+                
+         // Grade 6
+        'data/grade6/g6science-week02.js',
+        'data/grade6/g6science-week03.js',
+        'data/grade6/g6science-week04.js',
+        'data/grade6/g6science-week05.js',
+        'data/grade6/g6science-week07.js',
+        'data/grade6/g6science-week08.js',
+        'data/grade6/g6science-week09.js',
+        'data/grade6/g6science-week10.js',
+                       
+        // ... Add all other files here
+    ],
+    
+    // Extract grade, subject, and week from file path
+    parseFileInfo: function(filePath) {
+        const info = {
+            grade: null,
+            subject: null,
+            week: null
+        };
+        
+        // Extract grade
+        if (filePath.includes('grade1')) info.grade = '1';
+        else if (filePath.includes('grade2')) info.grade = '2';
+        else if (filePath.includes('grade3')) info.grade = '3';
+        else if (filePath.includes('grade4')) info.grade = '4';
+        else if (filePath.includes('grade5')) info.grade = '5';
+        else if (filePath.includes('grade6')) info.grade = '6';
+        else if (filePath.includes('baby')) info.grade = 'baby';
+        else if (filePath.includes('middle')) info.grade = 'middle';
+        else if (filePath.includes('reception')) info.grade = 'reception';
+        
+        // Extract subject
+        if (filePath.includes('-math-')) info.subject = 'Math';
+        else if (filePath.includes('-science-')) info.subject = 'Science';
+        else if (filePath.includes('-english-')) info.subject = 'English';
+        
+        // Extract week
+        const weekMatch = filePath.match(/week(\d+)/);
+        if (weekMatch) info.week = weekMatch[1];
+        
+        return info;
+    },
+    
+    // Group files for easier loading
+    getFilesByFilter: function(filters) {
+        return this.files.filter(filePath => {
+            const fileInfo = this.parseFileInfo(filePath);
+            
+            // Filter logic based on file info
+            if (filters.grade && filters.grade !== 'all') {
+                if (fileInfo.grade !== filters.grade) return false;
+            }
+            
+            if (filters.subject && filters.subject !== 'all') {
+                if (fileInfo.subject !== filters.subject) return false;
+            }
+            
+            if (filters.week && filters.week !== 'all') {
+                if (fileInfo.week !== filters.week) return false;
+            }
+            
+            return true;
+        });
+    },
+    
+    // Get all available options from manifest
+    getAllOptions: function() {
+        const options = {
+            grades: new Set(),
+            subjects: new Set(),
+            weeks: new Set()
+        };
+        
+        this.files.forEach(filePath => {
+            const info = this.parseFileInfo(filePath);
+            if (info.grade) options.grades.add(info.grade);
+            if (info.subject) options.subjects.add(info.subject);
+            if (info.week) options.weeks.add(info.week);
+        });
+        
+        return options;
+    }
+};
+
+window.WorksheetManifest = WorksheetManifest;
