11469ee33fa42a44e2814553ab390f142a11b96f
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2026-01-23 07:10:08 +0200

    Add files via upload

--- worksheets-system/data/manifest.js
+++ worksheets-system/data/manifest.js
@@ -64,6 +64,15 @@ const WorksheetManifest = {
         'data/grade1/g1english-t1week08.js',
         'data/grade1/g1english-t1week09.js',
         'data/grade1/g1english-t1week10.js',
+        //CTS
+        'data/grade1/g1creativeTech-t1week02.js',
+        'data/grade1/g1creativeTech-t1week03.js',
+        'data/grade1/g1creativeTech-t1week04.js',
+        'data/grade1/g1creativeTech-t1week05.js',
+        'data/grade1/g1creativeTech-t1week07.js',
+        'data/grade1/g1creativeTech-t1week08.js',
+        'data/grade1/g1creativeTech-t1week09.js',
+        'data/grade1/g1creativeTech-t1week10.js',
     
     
     // ************* Grade 2 *************
